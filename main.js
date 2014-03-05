182591d920574df4c891d39e81e82d4b658919be
Gordon Brander <marlfoskr@gmail.com>
2014-03-05 16:18:04 -0700

    Begin work differing cold from hot starts

--- main.js
+++ main.js
@@ -1,24 +1,19 @@
-// App
-// -----------------------------------------------------------------------------
+var emailWindowEl = document.getElementById('sys-window-email');
+var windowCol = document.querySelectorAll('.sys-window');
+var homeWindowEl = document.getElementById('sys-window-home');
+var windowsEl = document.getElementById('sys-windows');
 
-function app() {
-  var emailWindowEl = document.getElementById('sys-window-email');
-  var homeWindowEl = document.getElementById('sys-window-home');
-  var windowsEl = document.getElementById('sys-windows');
+var to = true;
 
-  var to = true;
-  window.addEventListener('click', function () {
-    if (to) {
-      windowsEl.classList.remove('sys-animate-from-home');
-      windowsEl.classList.add('sys-animate-to-home');
-    }
-    else {
-      windowsEl.classList.remove('sys-animate-to-home');
-      windowsEl.classList.add('sys-animate-from-home');
-    }
+window.addEventListener('click', function () {
+  if (to) {
+    windowsEl.classList.remove('anim-to-app-from-home');
+    windowsEl.classList.add('anim-to-home');
+  }
+  else {
+    windowsEl.classList.remove('anim-to-home');
+    windowsEl.classList.add('anim-to-app-from-home');
+  }
 
-    to = !to;
-  });
-}
-
-app();
+  to = !to;
+});
