b6e639ef54dbd1221388c48f1d3bbdfcaf623ae7
Gordon Brander <marlfoskr@gmail.com>
2014-03-06 10:26:06 -0700

    Basic "appear" working for new app

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-var emailWindowEl = document.getElementById('sys-window-email');
-var windowCol = document.querySelectorAll('.sys-window');
-var homeWindowEl = document.getElementById('sys-window-home');
-var windowsEl = document.getElementById('sys-windows');
+var screenEl = document.getElementById('sys-screen');
 
 var to = true;
 
 window.addEventListener('click', function () {
   if (to) {
-    windowsEl.classList.remove('state-to-app-from-home');
-    windowsEl.classList.add('state-to-home');
+    screenEl.classList.remove('state-to-app-from-home');
+    screenEl.classList.add('state-to-home');
   }
   else {
-    windowsEl.classList.remove('state-to-home');
-    windowsEl.classList.add('state-to-app-from-home');
+    screenEl.classList.remove('state-to-home');
+    screenEl.classList.add('state-to-app-from-home');
   }
 
   to = !to;
