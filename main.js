3d53be50a8ecc85808c65a344c24f443e0c41478
Gordon Brander <marlfoskr@gmail.com>
2014-03-06 09:50:13 -0700

    Switch to "state" change language

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ var to = true;
 
 window.addEventListener('click', function () {
   if (to) {
-    windowsEl.classList.remove('anim-to-app-from-home');
-    windowsEl.classList.add('anim-to-home');
+    windowsEl.classList.remove('state-to-app-from-home');
+    windowsEl.classList.add('state-to-home');
   }
   else {
-    windowsEl.classList.remove('anim-to-home');
-    windowsEl.classList.add('anim-to-app-from-home');
+    windowsEl.classList.remove('state-to-home');
+    windowsEl.classList.add('state-to-app-from-home');
   }
 
   to = !to;
